556e64a4090fafa48f293b0a581fbdbe771158ee
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2022-07-30 00:07:20 +0300

    Time: 3 ms (69.23%), Space: 44.2 MB (11.87%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+       Set<Integer> seen = new HashSet<Integer>();
+        
+        for(int i : arr){
+            if(seen.contains(i*2) || (i % 2 == 0 && seen.contains(i/2))){
+                return true;
+            }
+                
+            seen.add(i);
+        }
+        
+        return false;
+    }
+}
